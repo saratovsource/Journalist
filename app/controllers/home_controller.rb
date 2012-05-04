b382533dd8111fd4c003cb0f3058d924fa6f2e7c
Kirillov Alexander <saratovsource@gmail.com>
2012-05-04 17:13:22 +0400

    try resolve problem with nil elements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ::BaseController
 
   def index
-    @items = TimeLine.feed.page(params[:page]).per(10).map(&:timelinable)
+    @items = TimeLine.feed.page(params[:page]).per(10).map(&:timelinable).compact
   end
 
   def sections
