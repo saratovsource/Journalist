baca6ccd9a97ebb1ee14d1e1be68d74d056b49f1
Kirillov Alexander <saratovsource@gmail.com>
2012-05-17 02:16:40 +0400

    add 404 page

--- app/controllers/rendering_controller.rb
+++ app/controllers/rendering_controller.rb
@@ -3,13 +3,21 @@ class RenderingController < ::BaseController#ApplicationController
   before_filter :retrive_route_object
 
   def show
-    @page_class = css_class_from(@routerable)
-    @comments = @routerable.comments || []
+    if @routerable.present?
+      @page_class = css_class_from(@routerable)
+      #@comments = @routerable.comments || []
 
-    if params.has_key? :like
-      @routerable.rate_and_save(1, @rater) unless @routerable.rated_by?(@rater)
-      render :text => (render_cell "buttons", :like, @routerable)
+      if params.has_key? :like
+        @routerable.rate_and_save(1, @rater) unless @routerable.rated_by?(@rater)
+        render :text => (render_cell "buttons", :like, @routerable)
+      end
+    else
+      render :page_not_found, :layout => "errors", :status => 404
     end
+  end
+
+  def page_not_found
+
   end
 
   def sections
@@ -20,6 +28,7 @@ class RenderingController < ::BaseController#ApplicationController
 
   def retrive_route_object
     @routerable = current_site.find_object_by_path("/#{params[:path]}") if params[:path]
+    return if @routerable.blank?
     @presenter = Journalist::Presenters::Presenter.build(@routerable)
     @layout_sections = @presenter.cells if @presenter.present?
     @rater = Rater.find_or_create_by(:ip_address => request.env['REMOTE_ADDR'], :site_id => current_site.id)
