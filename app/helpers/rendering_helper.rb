1a27c41689a0df49a5185dabd71bc38ae06a6af3
Kirillov Alexander <saratovsource@gmail.com>
2012-06-16 21:22:08 +0400

    add item seo

--- app/helpers/rendering_helper.rb
+++ app/helpers/rendering_helper.rb
@@ -21,6 +21,54 @@ module RenderingHelper
   def name_of_user(account)
     account.profile.name || account.name || t(:anonymous)
   end
+
+  def seo_title(item)
+    title_text = ""
+    if item.respond_to?(:seo_title) && item.seo_title.present?
+      title_text =  item.seo_title if item.seo_title.present?
+    else
+      title_text = item.title.j_clean
+    end
+    content_tag(:title, [title_text, I18n.t(:site_name) ].join(I18n.t(:title_delimiter)))
+  end
+
+  def seo_description(item)
+    description_text = ""
+    if item.respond_to?(:meta_description) && item.meta_description.present?
+      description_text = item.meta_description
+    else
+      description_text = item.description if item.respond_to?(:description)
+    end
+    if description_text.blank?
+      nil
+    else
+      tag(:meta, :name => "description", :content => description_text)
+    end
+  end
+
+  def seo_tags(item)
+    ret_value = ""
+    if item.respond_to?(:meta_keywords) && item.meta_keywords.present?
+      ret_value = item.meta_keywords
+    else
+      ret_value = item.tags.join(', ') if item.respond_to?(:tags)
+    end
+    if ret_value.blank?
+      nil
+    else
+      tag(:meta, :name => "keywords", :content => ret_value)
+    end
+  end
+
+  def seo_author(item)
+    ret_value = ""
+    ret_value = item.owner.name if (item.owner.name.present?)
+    if ret_value.blank?
+      nil
+    else
+      tag(:meta, :name => "author", :content => ret_value)
+    end
+  end
   protected
 
   def has_valid_keys?(section, keys=[:name, :action])
