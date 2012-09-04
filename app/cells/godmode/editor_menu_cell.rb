4c283030404a80e947ad7a992b88eb354312201e
Kirillov Alexander <saratovsource@gmail.com>
2012-09-04 21:29:45 +0400

    small fix in godmode and main pages

--- app/cells/godmode/editor_menu_cell.rb
+++ app/cells/godmode/editor_menu_cell.rb
@@ -3,10 +3,11 @@ class Godmode::EditorMenuCell < ::MenuCell
   protected
 
   def build_list
-    add :journal,   :url => prepublished_godmode_journal_articles_path, :controllers => [Godmode::JournalArticlesController]
-    add :face_patrol, :url => prepublished_godmode_faces_path, :controllers => [Godmode::FacesController]
-    add :labels,    :url => godmode_labels_path, :controllers => [Godmode::LabelsController]
-    add :pages,     :url => godmode_pages_path, :controllers => [Godmode::PagesController]
+    abil = Ability.new(current_account, current_site)
+    add :journal,   :url => prepublished_godmode_journal_articles_path, :controllers => [Godmode::JournalArticlesController] if abil.can? :manage, JournalArticle
+    add :face_patrol, :url => prepublished_godmode_faces_path, :controllers => [Godmode::FacesController] if abil.can? :manage, Face
+    add :labels,    :url => godmode_labels_path, :controllers => [Godmode::LabelsController] if abil.can? :manage, Label
+    add :pages,     :url => godmode_pages_path, :controllers => [Godmode::PagesController] if abil.can? :manage, Page
   end
 
   def build_item(name, attributes)
